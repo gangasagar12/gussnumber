e0962e445dd077b8a639d83acb3bc6500ccb1ce1
Ganga Sagar Joshi <197456338+gangasagar12@users.noreply.github.com>
2025-06-23 13:04:47 +0545

    new updated

--- gussnumber.py
+++ gussnumber.py
@@ -1,13 +1,19 @@
 import random
-n=random.randint(1,100)
-a=-1
-gusses=0
-while(a!=n):
-    gusses+=1
-    a=int(input("guss a number: "))
-    if (a>n):
-        print("lower number please")
-    else:
-        print("higher number please")
-
-print(f" you have gusses the correct number in a t { gusses}")
+import os
+def load_high_scores():
+    if os.path.exists("high_scores.txt"):
+        with open("high_scores.txt", "r") as file:
+            return [line.strip() for line in file.readlines()]
+    return []
+def main():
+    n=random.randint(1,100)
+    a=-1
+    gusses_count=0
+    #  set up the maximum guss limit here
+    max_gusses = 10
+    gusses_history=[]   
+    #  list to store the gusses history
+    print("welcome the game of gusses number ")
+    while a!=n and gusses_count < max_gusses:
+        gusses_count+=1
+        #  increament the gusses count
